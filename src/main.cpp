3f359506109916e3401fccd8af379135c314d658
Stroby241 <maarten.behn@gmail.com>
2021-01-21 15:43:19 +0100

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,53 +4,45 @@ typedef struct vec3 {
     int x, y, z;
 } vec3;
 
-#define X_Pins_Size 9
-int X_Pins[X_Pins_Size] = {4,5,6,7,8,9,12,11,10};
+#define X_Pins_Size 9 // Amount of X Dimention Pins
+int X_Pins[X_Pins_Size] = {4,5,6,7,8,9,12,11,10}; // The pin ids of X Dimention
 
-#define Y_Pins_Size 3
-int Y_Pins[Y_Pins_Size] = {A1,A0,A2};
+#define Y_Pins_Size 3 // Amount of Y Dimention Pins
+int Y_Pins[Y_Pins_Size] = {A1,A0,A2}; // The pin ids of Y Dimention
 
-#define Button_Pins_Size 7
-int Button_Pins[Button_Pins_Size] = {A5,A4,2,A6,3,A7,A3};
+#define Button_Pins_Size 7 // Amount of button pins
+int Button_Pins[Button_Pins_Size] = {A5,A4,2,A6,3,A7,A3}; // The button pins
 
-#define Board_Size 3
-int board[3][3][3];
-vec3 head;
-int aktiveButton = 0; // 0 x+, 1 x-, 2 y+, 3 y-, 4 z+, 5 z-,
-int snakeLenght;
-bool newApple;
-bool death;
+#define Board_Size 3 // Size of the bord amount of leds is this ^3
+int board[3][3][3]; // The bord. 0 = empti cell, -1 = apple, 1 = head, >1 = body
+vec3 head; // position of head
+int aktivButton = 0; // 0 x+, 1 x-, 2 y+, 3 y-, 4 z+, 5 z-,
+int snakeLenght; // the length of the snake 
+bool newApple; // flag if new apple needs to be placed.
+bool death; // flag if the player is death and the game can be restarted. Starts with true.
 
-vec3 emptiCells[Board_Size * Board_Size * Board_Size];
-int emptiCellPointer;
+vec3 emptiCells[Board_Size * Board_Size * Board_Size]; // A list of all empti cells for apple placement.
+int emptiCellPointer; // A random index of the empti cell list for apple placement.
 
+// blink timer
 #define Matrix_Delay 1
 #define Max_Head_Counter 50
 #define Max_Apple_Counter 100
 #define Max_Death_Counter 300
+// The bord game as timer version for draw call.
 int matrix[3][3][3];
-int matrixCounter = 0;
+int matrixCounter = 0; // Counter for delay timer.
 #define Max_Matrix_Counter 10000
 
-void setAllLEDS(bool state){
-  for (size_t x = 0; x < Board_Size; x++)
-  {
-    for (size_t y = 0; y < Board_Size; y++)
-    {
-      for (size_t z = 0; z < Board_Size; z++)
-      {
-        matrix[x][y][z] = state;
-      }
-    }
-  }
-}
-
+// Rests the game
 void newGame(){
+  // Clears varibles
   head = {1,1,1};
   snakeLenght = 1;
-  aktiveButton = 1;
+  aktivButton = 1;
   newApple = 1;
 
+  // Clears board
   for (size_t x = 0; x < Board_Size; x++)
   {
     for (size_t y = 0; y < Board_Size; y++)
@@ -61,10 +53,12 @@ void newGame(){
       }
     }
   }
+  // Places head
   board[head.x][head.y][head.z] = 1;
-  death = 0;
+  death = 0; // Sets death to 0 so the game is running.
 }
 
+// Converts the bord data to the matrix wiith the corect blink timers.
 void pushMatrix(){
   for (size_t x = 0; x < Board_Size; x++)
   {
@@ -91,9 +85,10 @@ void pushMatrix(){
   }
 }
 
+// The main game loop
 void gameLoop(){
 
-  // Set move Direction from pressed buttons
+  // Sets aktivButton a varible which says what button was pressed last.
   for (size_t i = 0; i < Button_Pins_Size; i++)
   {
     bool button;
@@ -105,11 +100,21 @@ void gameLoop(){
 
     if(button)
     {
-      aktiveButton = i;
+      aktivButton = i;
     }
   }
 
-  switch (aktiveButton){ // Move head in move direction.
+  // Button to restart game.
+  if(aktivButton = 6){
+    newGame();
+    return;
+  }
+  else if(death){ // Stop here if the palyer is death
+    return;
+  }
+
+  // Move the head in the direction aktivButton says..
+  switch (aktivButton){ 
   case 0:
     head.x++;
     if(head.x > Board_Size - 1)
@@ -152,13 +157,6 @@ void gameLoop(){
       head.z = Board_Size - 1;
     }
     break;
-  case 6:
-    newGame();
-    return;
-  }
-
-  if(death){
-    return;
   }
 
   if(board[head.x][head.y][head.z] < 0) // Check if head moven on apple cell.
